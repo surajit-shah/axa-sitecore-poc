778f951a52b694f3ece2ffa996e7e4b492c0ca94
Ezhilarasan Rangan <ezhil.r.it@gmail.com>
2019-05-19 22:45:10 +0200

    Unicorn serialization configuration perfected

--- src/Feature/Metadaa/code/Models/Metadaa.cs
+++
@@ -1,7 +0,0 @@
-namespace Mirabeau.Feature.Metadaa.Models
-{
-  public class Metadaa
-  {
-    // TODO: Implement model or otherwise remove this file
-  }
-}
--- src/Foundation/Serialization/code/Models/Serialization.cs
+++
@@ -1,7 +0,0 @@
-namespace Mirabeau.Foundation.Serialization.Models
-{
-  public class Serialization
-  {
-    // TODO: Implement model or otherwise remove this file
-  }
-}
